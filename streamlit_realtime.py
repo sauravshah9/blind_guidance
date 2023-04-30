e74e9d7eb3eef7c8085bdd7e04ef93e6753d76b3
sauravshah9 <78735484+sauravshah9@users.noreply.github.com>
2023-04-30 20:12:24 +0530

    Add files via upload

---
+++ streamlit_realtime.py
@@ -0,0 +1,126 @@
+import streamlit as st
+import cv2
+import numpy as np
+import urllib.request
+import time
+import pyttsx3
+
+
+# engine = pyttsx3.init()
+
+def object_detection_realtime():
+    CONFIDENCE = 0.5
+    SCORE_THRESHOLD = 0.5
+    IOU_THRESHOLD = 0.5
+    config_path = 'yolov3.cfg'
+    weights_path = 'yolov3.weights'
+    font_scale = 1
+    thickness = 1
+    url = "https://raw.githubusercontent.com/zhoroh/ObjectDetection/master/labels/coconames.txt"
+    f = urllib.request.urlopen(url)
+    labels = [line.decode('utf-8').strip() for line in f]
+
+    colors = np.random.randint(0, 255, size=(len(labels), 3), dtype="uint8")
+
+    net = cv2.dnn.readNetFromDarknet(config_path, weights_path)
+
+    ln = net.getLayerNames()
+    ln = [ln[i - 1] for i in net.getUnconnectedOutLayers()]
+    st.title("Real-time Object Detection")
+    st.subheader("""
+    This app detects objects in real-time from your webcam feed and displays the result in real-time
+    """)
+    cap = cv2.VideoCapture(0)
+       
+    my_placeholder = st.empty()
+    while True:
+        # _, image = cap.read()
+        # h, w = image.shape[:2]x
+        
+        re,image = cap.read()
+        image = cv2.resize(image, None, fx=0.4, fy=0.4)
+        h, w, channels = image.shape
+        
+        blob = cv2.dnn.blobFromImage(image, 1/255.0, (416, 416), swapRB=True, crop=False)
+        net.setInput(blob)
+        layer_outputs = net.forward(ln)
+
+        boxes, confidences, class_ids = [], [], []
+
+        # loop over each of the layer outputs
+        for output in layer_outputs:
+            # loop over each of the object detections
+            for detection in output:
+                # extract the class id (label) and confidence (as a probability) of
+                # the current object detection
+                scores = detection[5:]
+                class_id = np.argmax(scores)
+                confidence = scores[class_id]
+                # discard weak predictions by ensuring the detected
+                # probability is greater than the minimum probability
+                if confidence > CONFIDENCE:
+                    # scale the bounding box coordinates back relative to the
+                    # size of the image, keeping in mind that YOLO actually
+                    # returns the center (x, y)-coordinates of the bounding
+                    # box followed by the boxes' width and height
+                    box = detection[:4] * np.array([w, h, w, h])
+                    (centerX, centerY, width, height) = box.astype("int")
+
+                    # use the center (x, y)-coordinates to derive the top and
+                    # and left corner of the bounding box
+                    x = int(centerX - (width / 2))
+                    y = int(centerY - (height / 2))
+
+                    # update our list of bounding box coordinates, confidences,
+                    # and class IDs
+                    boxes.append([x, y, int(width), int(height)])
+                    confidences.append(float(confidence))
+                    class_ids.append(class_id)
+
+        # perform the non maximum suppression given the scores defined before
+        idxs = cv2.dnn.NMSBoxes(boxes, confidences, SCORE_THRESHOLD, IOU_THRESHOLD)
+
+        font_scale = 0.6
+        thickness = 1
+
+        # ensure at least one detection exists
+        if len(idxs) > 0:
+            # loop over the indexes we are keeping
+            for i in idxs.flatten():
+                # extract the bounding box coordinates
+                x, y = boxes[i][0], boxes[i][1]
+                w, h = boxes[i][2], boxes[i][3]
+
+                # draw the bounding box rectangle and label on the image
+                color = [int(c) for c in colors[class_ids[i]]]
+                cv2.rectangle(image, (x, y), (x + w, y + h), color=color, thickness=thickness)
+                text = f"{labels[class_ids[i]]}: {confidences[i]:.2f}"
+                (text_width, text_height) = cv2.getTextSize(text, fontFace=cv2.FONT_HERSHEY_SIMPLEX, fontScale=font_scale, thickness=thickness)[0]
+                text_offset_x = x
+                text_offset_y = y - 5
+                box_coords = ((text_offset_x, text_offset_y), (text_offset_x + text_width + 2, text_offset_y - text_height))
+                cv2.rectangle(image, box_coords[0], box_coords[1], color=color, thickness=cv2.FILLED)
+                cv2.putText(image, text, (text_offset_x, text_offset_y), fontFace=cv2.FONT_HERSHEY_SIMPLEX, fontScale=font_scale, color=(0, 0, 0), thickness=thickness)
+#                 engine.say(text)
+
+#                 engine.runAndWait()
+    # Display the resulting frame
+        my_placeholder.image(image, use_column_width=True)
+        # time.sleep(0.1)
+
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+                break 
+
+# Release the camera and destroy all windows
+    cap.release()
+    cv2.destroyAllWindows()
+
+if __name__ == '__main__':
+	object_detection_realtime()
+
+
+
+
+
+
